2d2df77e5af5a54b90c95b42942f962702787da5
User <tu7g0aq9h@code.edu.az>
2022-11-29 10:22:30 +0400

    usernameandpasswordcheck

---
+++ classwork29_11/Models/User.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork29_11.Models
+{
+    internal class User
+    {
+        private string _name;
+        private string _password;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (usernamevalidator(value))
+                {
+                    _name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong username ");
+                }
+            }
+        }
+        public User(string name , string password)
+        {
+            Name = name;
+            Password = password;
+        }
+        public string Password
+        {
+            get { return _password; }
+            set 
+            {
+                if (passwordvalidator(value))
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("wrong password");
+                }
+            }
+        }
+        public bool usernamevalidator(string name)
+        {
+              
+            for (int i = 0; i < name.Length; i++)
+            {
+                if (!char.IsLetter(name[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool passwordvalidator(string surname)
+        {
+            bool result = false;
+            if (surname.Length > 8)
+            {
+                bool result1 = false;
+                bool result2 = false;
+                bool result3 = false;
+                for (int i = 0; i < surname.Length; i++)
+                {
+                    if (char.IsDigit(surname[i]))
+                    {
+                        result1 = true;
+                    }
+                    if (char.IsUpper(surname[i]))
+                    {
+                        result2 = true;
+                    }
+                    if (char.IsLower(surname[i]))
+                    {
+                        result3 = true;
+                    }
+                }
+                if(result1==true && result2==true && result3 == true)
+                {
+                    result = true;
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ classwork29_11/Program.cs
@@ -0,0 +1,14 @@
+﻿using classwork29_11.Models;
+using System;
+
+namespace classwork29_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("fateh" , "saLam3123");
+            Console.WriteLine("Username : " + user.Name + "\n" + "Password : " + user.Password); ;
+        }
+    }
+}
